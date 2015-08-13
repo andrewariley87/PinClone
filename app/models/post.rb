9dc6021c476f4cc5a8c6a8645500ebc185727fcb
andrewariley87 <andrewariley87@gmail.com>
2015-08-13 14:35:59 -0400

    validations
    post now belongs to a user, gifs cannot be submitted without ending in .gif

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+  validate :gif_has_correct_format
+
+  def gif_has_correct_format
+    errors.add(:gif, "format is incorrect, make sure file name ends with .gif") unless gif.downcase.ends_with?('.gif')
+  end
 end
