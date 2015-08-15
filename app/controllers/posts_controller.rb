969333d014e3b6117041d258193264d57c101445
andrewariley87 <andrewariley87@gmail.com>
2015-08-15 16:00:52 -0400

    modals
    I have one working modal, but it is messing with the formatiing on the page, I have tried to make the other modal, but I am running into an error message because the User is nil on @post so I cannot call email on it

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,14 +25,15 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-
+    @post.user_id = current_user.id
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
+        format.js{}
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.js {}
       end
     end
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,4 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  validate :gif_has_correct_format
-
-  def gif_has_correct_format
-    errors.add(:gif, "format is incorrect, make sure file name ends with .gif") unless gif.downcase.ends_with?('.gif')
-  end
+  validates :gif, format: { with: /\.gif\z/, message: "File must be a gif, must end with .gif" }
 end
